06d561714b9bdda089eea99ab5ab3ceafec7b108
Brudar Artem Sergeevich <71312371+progagoda@users.noreply.github.com>
2023-11-11 13:26:41 +0300

    add dark theme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 // import { MainPage } from "./pages/mainPage";
-import { MapContainer, Marker, Popup, TileLayer, CircleMarker, Tooltip } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, CircleMarker, Tooltip, Polyline } from 'react-leaflet';
 import ApiService from './ApiService.js';
 
 function App() {
   const apiService = new ApiService("http://localhost:5000/api");
+    const routeCoordinates = [
+        [59.57, 30.19],
+        [51.51, -0.1],
+        // Добавьте дополнительные координаты для вашего маршрута
+    ];
 
 
   const [stations, setStations] = useState([]);
@@ -43,21 +48,21 @@ function App() {
 
 
   return (
-    <MapContainer center={[59.57, 30.19]} zoom={13} scrollWheelZoom={true} style={{ height: "100vh", width: "100vw" }}>
+    <MapContainer center={[59.57, 30.19]} zoom={13} scrollWheelZoom={true} style={{ height: "100vh", width: "100vw"}}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
       />
       {stations && stations.map(([station_id, lon, lat]) =>
         <CircleMarker // - железнодорожные станции
           center={[lon, lat]}
-          pathOptions={{ color: 'red' }}
+          pathOptions={{ color: '#FFF' }}
           radius={6}>
           <Tooltip>{station_id}</Tooltip>
         </CircleMarker>
       )}
       {vagnums && vagnums.map(({ station, wagnum }) => (
-        <Marker position={[station["LATITUDE"],station["LONGITUDE"]]}>
+        <Marker position={[station["LATITUDE"],station["LONGITUDE"]] icon={markerIcon}}>
           <Popup>
               <div>
                   <p className="dictionaryWagon">
@@ -80,6 +85,7 @@ function App() {
         </Marker>
       )
       )}
+        <Polyline positions={routeCoordinates} color="#EB5525" />
     </MapContainer>
   )
 }
